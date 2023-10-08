e997733766428a66c5aac78937dfc5125e09f3c0
cuong19962012 <tancuong.it@gmail.com>
2023-10-08 20:25:19 +0700

    [Sound Hub]update router

--- font_end/src/App.js
+++ font_end/src/App.js
@@ -3,9 +3,17 @@ import { Home } from "./components/Home";
 import { Login } from "./components/Login";
 import { Album } from "./components/Album";
 import './style/style.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <Detail />
+    <BrowserRouter>
+      <Routes>
+        <Route path="detail/:id" element={<Detail />} />
+        <Route path="login" element={<Login />} />
+        <Route path="album" element={<Album />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
